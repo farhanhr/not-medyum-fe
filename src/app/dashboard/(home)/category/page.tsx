24f07c9b701c9601c5b9856b0e3d025482633b15
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-06-13 17:50:06 +0700

    setup fetching category and error alert

--- src/app/dashboard/(home)/category/page.tsx
+++ src/app/dashboard/(home)/category/page.tsx
@@ -2,11 +2,49 @@
 
 import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
-import { Plus } from "lucide-react";
+import { AlertCircle, Plus } from "lucide-react";
 import Link from "next/link";
+import axiosInstance, { setupInterceptor } from "../../../../../lib/axios";
+import { useEffect, useState } from "react";
+import { Category } from "@/model/Category";
+import { ApiResponse } from "@/model/ApiResponse";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function CategoryPage() {
-    
+    setupInterceptor();
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string |null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axiosInstance.get<ApiResponse<Category[]>>("/admin/categories");
+                setCategories(response.data.data);
+                setLoading(false);
+            } catch (error: any) {
+                setError(error.message || "Error fetching data");
+                setLoading(false);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    if (error) return (
+        <Alert variant="destructive">
+            <AlertCircle className="h-4 w-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+                {error}
+            </AlertDescription>
+        </Alert>
+    )
+
     return (
         <>
         <div className="flex flex-row items-center justify-between">
